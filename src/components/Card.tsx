59da8c85d2b7894a5e29b10f03fad428efcb7918
Vinicius <vi058590@intelbras.com.br>
2023-11-20 11:57:54 -0300

    chore: update to receive the entire form

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,20 @@
-interface ICard {
-  id: number,
-  paragraph: string,
-  details: string
-}
+import { Box, Center, Input } from "@chakra-ui/react"
+import { ButtonComponent } from "./Button.component"
+import { login } from "../services/login"
 
-export const Card = ({ id, paragraph, details }: ICard) => {
+export const Card = () => {
   return(
-    <div>
-      <h1>Card {id}</h1>
-      <p>{paragraph}</p>
-      <p>{details}</p>
-    </div>
+    <Box minHeight='100vh' backgroundColor='#9413dc' padding='25px'>
+      <Box backgroundColor='#FFFFFF' borderRadius='25px' padding='15px' >
+        <Center>
+          <h1>Faça o login</h1>
+        </Center>
+        <Input placeholder="email" />
+        <Input placeholder="password" />
+        <Center>
+          <ButtonComponent onClick={login} text="Button" chackraProps={{colorScheme:'teal', size:'sm', width:'100%', marginTop:'5px'}} />
+        </Center>
+      </Box>
+    </Box>
   )
 }
