a02ec1897c9c00140a2b17f23d6e97c00dfbde85
Alisson Rego <alissondroid07@gmail.com>
2024-02-13 10:53:36 -0300

    Refatorar o componente de Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,14 @@
-import { 
-  Center,
-  ChakraProvider,
-  Input,
-  Box
-} from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import { Header } from './components/Header';
-import { Botao } from './components/Botao';
 
 import { login } from './services/login';
+import { Card } from './components/Card';
 
 function App() {
   return (
     <ChakraProvider>
       <Header title={'Dio Bank'} />
-      <Box minHeight='100vh' backgroundColor='#9413dc' padding='25px'>
-        <Box backgroundColor='#FFFFFF' borderRadius='25px' padding='15px' >
-          <Center>
-            <h1>Faça o login</h1>
-          </Center>
-          <Input placeholder="email" />
-          <Input placeholder="password" />
-          <Center>
-            <Botao title={'Login'} event={login} />
-          </Center>
-        </Box>
-      </Box>
+      <Card event={login} />
     </ChakraProvider>
   );
 }
